d37a64a035e91c6ca53ac8c2ab7b8d639941ed74
sharifulbony <shariful.bony@gmail.com>
2019-10-26 02:47:03 +0300

    restructuring and code cleanup

--- src/main/java/com/sharifulbony/tokenlogin/jwt/JwtRequest.java
+++ src/main/java/com/sharifulbony/tokenlogin/jwt/JwtRequest.java
@@ -7,6 +7,9 @@ public class JwtRequest implements Serializable {
     private String username;
     private String password;
 
+    public JwtRequest(){
+
+    }
     public JwtRequest(String username, String password) {
         this.setUsername(username);
         this.setPassword(password);
